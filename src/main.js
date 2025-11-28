cbf46ce694dcc72f15846ea52bbda5aa8e35e2e0
Peter Pettas <peterpettas@gmail.com>
2025-11-28 16:19:39 +1100

    chore: set base styles, variables and assets

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import './scss/main.scss';
 
-import { initSlider } from './js/slider.js';
-import { initViewportAnimations } from './js/animations.js';
+// import { initSlider } from './js/slider.js';
+// import { initViewportAnimations } from './js/animations.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    initSlider();
-    initViewportAnimations();
+    // initSlider();
+    // initViewportAnimations();
 });
 
 
